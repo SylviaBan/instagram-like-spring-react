5f522216b67c256715b2bacbb5f0edaee0057e87
Sylvia B <sylwiab@gmail.com>
2023-09-20 21:51:26 +0200

    Mise en place page Right Navbar.

---
+++ app/src/components/rightBar/RightBar.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import "./rightBar.css"
+
+const RightBar = () => {
+    return (
+        <div className="rightBarClass">
+            <div className="container">
+                <div className="item">
+                    <span>Ils vous ont contacté</span>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img
+                                src="https://images.pexels.com/photos/2218786/pexels-photo-2218786.jpeg?auto=compress&cs=tinysrgb&w=600"
+                                alt=""
+                            />
+                            <span>AnneClaire</span>
+                        </div>
+                        <div className="buttons">
+                            <button>Accepter</button>
+                            <button>Annuler</button>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img
+                                src="https://images.pexels.com/photos/14697557/pexels-photo-14697557.jpeg?auto=compress&cs=tinysrgb&w=600"
+                                alt=""
+                            />
+                            <span>Pierre Legendre</span>
+                        </div>
+                        <div className="buttons">
+                            <button>Accepter</button>
+                            <button>Annuler</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="user">
+                    <div className="userInfo">
+                        <img
+                            src="https://images.pexels.com/photos/2380794/pexels-photo-2380794.jpeg?auto=compress&cs=tinysrgb&w=600"
+                            alt=""
+                        />
+                        <span>Michaël1975</span>
+                    </div>
+                    <div className="buttons">
+                        <button>Accepter</button>
+                        <button>Annuler</button>
+                    </div>
+                </div>
+                <div className="user">
+                    <div className="userInfo">
+                        <img
+                            src="https://images.pexels.com/photos/1727273/pexels-photo-1727273.jpeg?auto=compress&cs=tinysrgb&w=600"
+                            alt=""
+                        />
+                        <span>Anne-Charlotte B.</span>
+                    </div>
+                    <div className="buttons">
+                        <button>Accepter</button>
+                        <button>Annuler</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default RightBar;
